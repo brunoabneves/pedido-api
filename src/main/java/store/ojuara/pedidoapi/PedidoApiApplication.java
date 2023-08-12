c7d379b6aa4d9824cee0270e1c44968a36701536
brunoabneves <brunoabneves@hotmail.com>
2023-08-11 23:17:21 -0300

    Iniciando projeto

---
+++ src/main/java/store/ojuara/pedidoapi/PedidoApiApplication.java
@@ -0,0 +1,13 @@
+package store.ojuara.pedidoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/store/ojuara/pedidoapi/domain/model/ModeloGenerico.java
@@ -0,0 +1,42 @@
+package store.ojuara.pedidoapi.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+public class ModeloGenerico implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private final UUID uuid = UUID.randomUUID();
+
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    private LocalDate updatedAt;
+
+    private String updateReason;
+}
