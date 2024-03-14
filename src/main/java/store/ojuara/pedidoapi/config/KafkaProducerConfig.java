71ca2c8a3c131ffcb25841b8746631f48b4daf43
brunoabneves <brunoabneves97@gmail.com>
2024-03-13 21:22:21 -0300

    OJLP-02 Criando classe Avro e ajustando KafkaProducerConfig

--- src/main/java/store/ojuara/pedidoapi/config/KafkaProducerConfig.java
+++ src/main/java/store/ojuara/pedidoapi/config/KafkaProducerConfig.java
@@ -1,17 +1,15 @@
 package store.ojuara.pedidoapi.config;
 
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,17 +23,22 @@ public class KafkaProducerConfig {
     @Value(value = "${mensageria.kafka.topic.pedido.processado}")
     private String topic;
 
-//    @Bean
-//    public NewTopic createTopic(){
-//        return new NewTopic(topic, 3, (short) 1);
-//    }
+    @Value("${spring.kafka.producer.properties.schema.registry.url}")
+    private String schemaUrl;
+
+    @Bean
+    public NewTopic createTopic(){
+        return new NewTopic(topic, 3, (short) 1);
+    }
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+
+        configProps.put("schema.registry.url",schemaUrl);
 
         return new DefaultKafkaProducerFactory<>(configProps);
     }
